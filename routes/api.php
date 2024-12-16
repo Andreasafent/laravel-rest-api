fceea9bd69ffbac339cfc7ed59723dd6d60bf6fc
Andreasafent <afentoulidisandreas@gmail.com>
2024-12-16 21:11:46 +0200

    added seperate file for routes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,18 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [AppController::class,'index']);
 
-Route::get('/products', [ProductController::class,'index']);
-Route::post('/products', [ProductController::class,'create']);
-Route::get('/products/{id}', [ProductController::class,'show']);
-Route::put('/products/{id}', [ProductController::class,'update']);
-Route::delete('/products/{id}', [ProductController::class,'delete']);
+// Route::get('/products', [ProductController::class,'index']);
+// Route::post('/products', [ProductController::class,'create']);
+// Route::get('/products/{id}', [ProductController::class,'show']);
+// Route::put('/products/{id}', [ProductController::class,'update']);
+// Route::delete('/products/{id}', [ProductController::class,'delete']);
 
+// Route::prefix("products")->group(function(){
+//     Route::get('', [ProductController::class,'index']);
+//     Route::post('', [ProductController::class,'create']);
+//     Route::get('{id}', [ProductController::class,'show']);
+//     Route::put('{id}', [ProductController::class,'update']);
+//     Route::delete('{id}', [ProductController::class,'delete']);
+// });
+
+Route::prefix("products")->group(base_path("routes/products.php"));
